2ff0b4252691fa61350d4dd13db7bef75c797054
Protsenko Maksim <net3311971@icloud.com>
2022-09-18 20:29:29 +0500

    homework

---
+++ src/objectshomework/Author.java
@@ -0,0 +1,27 @@
+package objectshomework;
+
+public class Author {
+    private final String name;
+    private final String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
